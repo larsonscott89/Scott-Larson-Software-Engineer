db60524b1cbc4e605111454c221bb83365be82f6
larsonscott89 <scottlarson0305@gmail.com>
2024-04-16 20:32:27 -0400

    new font

--- Personal-Website/src/components/Home.jsx
+++ Personal-Website/src/components/Home.jsx
@@ -2,9 +2,10 @@ import Homephoto from '../assets/PersonalWebsite:homephoto.png'
 
 export default function Home () {
   return(
-    <div>
-      <h1>Home</h1>
-      <img className='homephoto' src={Homephoto} alt='Home Page Photo'/>
+    <div className='home-container'>
+      <h1 className='full-name'> Scott Larson </h1>
+      <h5 className='brand-statement'> I am a Software Engineer who has a passion for creating and developing products for the average user. My previous careers in sales and the refined fuels industry helped me to develop a strong aptitude for problem solving, critical thinking, and leadership. My curiosity for learning and passion for coding has driven me to create programs that help people in their everyday lives. </h5>
+      <img className='home-photo' src={Homephoto} alt='Home Page Photo'/>
     </div>
   )
 }
