f8e0110d4b6cc4a935e8bbb857e36a3b50daf262
larsonscott89 <scottlarson0305@gmail.com>
2024-04-16 21:44:18 -0400

    spotlight added

--- Personal-Website/src/components/Header.jsx
+++ Personal-Website/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import Nav from './Nav'
+import HeaderImg from '../assets/Header Background.png'
 
 export default function Header() {
   return (
     <div className='header'>
+      <img className='header-img' src={HeaderImg}/>
       <Nav />
     </div>
   )
